af8e04930e2a554cd079351623e278b0a808fbe7
Luke Munn <lmunn64@gmail.com>
2024-06-05 23:52:54 -0500

    full 3d render of caruso cube orbiting other caruso

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -11,38 +11,88 @@
 #include <glm/glm/gtc/type_ptr.hpp>
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 960;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 //Normalized Device Coordinates 
 //-----------------------------
+//float vertices[] = {
+//	 //positions		 //colors           //texture coords
+//	 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
+//	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+//	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, 
+//	-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f
+//};
+
 float vertices[] = {
-	 //positions		 //colors           //texture coords
-	 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
-	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
-	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, 
-	-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f
+		-0.2f, -0.2f, -0.2f,  0.0f, 0.0f,
+		 0.2f, -0.2f, -0.2f,  1.0f, 0.0f,
+		 0.2f,  0.2f, -0.2f,  1.0f, 1.0f,
+		 0.2f,  0.2f, -0.2f,  1.0f, 1.0f,
+		-0.2f,  0.2f, -0.2f,  0.0f, 1.0f,
+		-0.2f, -0.2f, -0.2f,  0.0f, 0.0f,
+
+		-0.2f, -0.2f,  0.2f,  0.0f, 0.0f,
+		 0.2f, -0.2f,  0.2f,  1.0f, 0.0f,
+		 0.2f,  0.2f,  0.2f,  1.0f, 1.0f,
+		 0.2f,  0.2f,  0.2f,  1.0f, 1.0f,
+		-0.2f,  0.2f,  0.2f,  0.0f, 1.0f,
+		-0.2f, -0.2f,  0.2f,  0.0f, 0.0f,
+
+		-0.2f,  0.2f,  0.2f,  1.0f, 0.0f,
+		-0.2f,  0.2f, -0.2f,  1.0f, 1.0f,
+		-0.2f, -0.2f, -0.2f,  0.0f, 1.0f,
+		-0.2f, -0.2f, -0.2f,  0.0f, 1.0f,
+		-0.2f, -0.2f,  0.2f,  0.0f, 0.0f,
+		-0.2f,  0.2f,  0.2f,  1.0f, 0.0f,
+
+		 0.2f,  0.2f,  0.2f,  1.0f, 0.0f,
+		 0.2f,  0.2f, -0.2f,  1.0f, 1.0f,
+		 0.2f, -0.2f, -0.2f,  0.0f, 1.0f,
+		 0.2f, -0.2f, -0.2f,  0.0f, 1.0f,
+		 0.2f, -0.2f,  0.2f,  0.0f, 0.0f,
+		 0.2f,  0.2f,  0.2f,  1.0f, 0.0f,
+
+		-0.2f, -0.2f, -0.2f,  0.0f, 1.0f,
+		 0.2f, -0.2f, -0.2f,  1.0f, 1.0f,
+		 0.2f, -0.2f,  0.2f,  1.0f, 0.0f,
+		 0.2f, -0.2f,  0.2f,  1.0f, 0.0f,
+		-0.2f, -0.2f,  0.2f,  0.0f, 0.0f,
+		-0.2f, -0.2f, -0.2f,  0.0f, 1.0f,
+
+		-0.2f,  0.2f, -0.2f,  0.0f, 1.0f,
+		 0.2f,  0.2f, -0.2f,  1.0f, 1.0f,
+		 0.2f,  0.2f,  0.2f,  1.0f, 0.0f,
+		 0.2f,  0.2f,  0.2f,  1.0f, 0.0f,
+		-0.2f,  0.2f,  0.2f,  0.0f, 0.0f,
+		-0.2f,  0.2f, -0.2f,  0.0f, 1.0f
 };
 
-
-unsigned int indices[] = {  // note that we start from 0!
-	0, 1, 3,   // first triangle
-	1, 2, 3    // second triangle
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  -0.0f),
+	glm::vec3(1.0f,  0.0f, -0.7f),
+	glm::vec3(-1.5f, -1.2f, -2.5f),
+	glm::vec3(-1.4f, -1.5f, -1.0f),
+	glm::vec3(2.4f, -0.2f, -3.5f),
+	glm::vec3(.7f,  1.0f, -1.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
 //Vertex Buffer Object to be generated
 unsigned int VBO[2];
 unsigned int VAO[2];
 
-unsigned int EBO;
 
 unsigned int shaderProgram;
 unsigned int shaderProgram2;
 
-unsigned int texture;
+unsigned int texture1;
 unsigned int texture2;
 
 int main() {
@@ -76,6 +126,8 @@ int main() {
 	}
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
+	glEnable(GL_DEPTH_TEST);
+
 	//Vertex shader compiling
 	Shader shader("shader.vs", "fragment.fs");
 
@@ -83,49 +135,44 @@ int main() {
 	glGenVertexArrays(2, VAO);
 	glGenBuffers(2, VBO);
 
-	//EBO generation
-	glGenBuffers(1, &EBO);
-
 	//First VAO
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s)
 	glBindVertexArray(VAO[0]);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//EBO binding
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	// texture attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	////EBO binding
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	 // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
 	//Load first texture image
-	glGenTextures(1, &texture);
+	glGenTextures(1, &texture1);
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glBindTexture(GL_TEXTURE_2D, texture1);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	//Texture set filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nrChannels;
 
 	stbi_set_flip_vertically_on_load(true);
 
-	unsigned char* data = stbi_load("heart.jpg", &width, &height, &nrChannels, 0);
+	unsigned char* data = stbi_load("caruso.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else {
@@ -140,9 +187,8 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	//Texture set filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
 
 	//Load second texture image
 	stbi_set_flip_vertically_on_load(true);
@@ -158,20 +204,15 @@ int main() {
 	}
 	stbi_image_free(data);
 
-
-	//Transformations
-	// 
-	glm::mat4 trans = glm::mat4(1.0f); //set the transformation
-	
-	unsigned int transformLocation = glGetUniformLocation(shader.ID, "transform");
-
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
 
 	shader.use();
-	shader.setInt("texture2", 1);
+	shader.setInt("texture1", 0);
 
+	
+	unsigned int locationReset = 2;
 	//Main render loop
 	//----------------
 	while (!glfwWindowShouldClose(window)) {
@@ -187,31 +228,60 @@ int main() {
 
 		//Rendering color
 		glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		//Transformations
-		glm::mat4 trans = glm::mat4(1.0f); //set the transformation
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		unsigned int transformLocation = glGetUniformLocation(shader.ID, "transform");
-
-		trans = glm::rotate(trans, (float)glfwGetTime() *4, glm::vec3(0.0, 0.0, 1.0));
-
-		trans = glm::translate(trans, glm::vec3(.5f, 0.0f, 0.0f));
-
-
-		//Rendering Triangle
-		// 
 		// bind textures on corresponding texture units
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+
+		//Activate shader
+		shader.use();
+
+		//Transformations
+		int modelLoc = glGetUniformLocation(shader.ID, "model");
+		int viewLoc = glGetUniformLocation(shader.ID, "view");
+		int projLoc = glGetUniformLocation(shader.ID, "projection");
+
+		glm::mat4 trans = glm::mat4(1.0f); //set the transformation
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 model2 = glm::mat4(1.0f);
+
+		glm::mat4 projection = glm::mat4(1.0f);
+		
+		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+		model2 = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.8f, 1.0f * (float)glfwGetTime()*8, 0.0f));
+
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(glm::radians(55.f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
 
 		//set transform uniform
-		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(trans));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		shader.use();
+
 		glBindVertexArray(VAO[0]);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
+		//first cube
+		model = glm::translate(model, cubePositions[0]);
+		float angle = 0.0f;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(3.0f, 0.3f, 0.5f));
+		
+		//set transform uniform
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	
+		//second cube
+		model2 = glm::translate(model2, cubePositions[1]);
+		angle = 40.0f * (float)glfwGetTime()*6;
+		model2 = glm::rotate(model2, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		
+		//set transform uniform
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
 
 		//glfw swap buffers and poll IO events
 		//------------------------------------
